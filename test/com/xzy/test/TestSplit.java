3646097d2438cdf1e24207c0ed8416b51857a7f1
blueskyxzy <blueskyxzy@gmail.com>
2019-02-26 18:45:24 +0800

    test split

--- test/com/xzy/test/TestSplit.java
+++ test/com/xzy/test/TestSplit.java
@@ -8,7 +8,7 @@ public class TestSplit {
 
     public static void main(String[] args){
         String str = "/杭州昌如/测试楼/1F/1F-2/";
-        String substr = str.substring(1, str.length());
+        String substr = str.substring(1);
         String[] strs = substr.split("/");
         System.out.println("size:" + strs.length);
         int i = 1;
