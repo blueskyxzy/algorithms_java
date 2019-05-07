8d425c8df8768085e9a5d18b9b3a91a56f5cde63
xzy <blueskyxzy@gmail.com>
2019-05-07 17:15:51 +0800

    add test code

--- nowcoder/com/xzy/nowcoder/jianzhioffer/Test04.java
+++ nowcoder/com/xzy/nowcoder/jianzhioffer/Test04.java
@@ -43,9 +43,9 @@ public class Test04 {
             int rootValue = pre[pstart];
             TreeNode rootNode = new TreeNode(rootValue);
             Integer index = inMap.get(rootValue);
-            // pstart + index - istart
+            // pstart + index - istart =  pstart + 1 + (index-1 - istart)
             TreeNode leftNode = constructprein(pre, in, pstart + 1, pstart + index - istart, istart, index - 1, inMap);
-            // pstart + index - istart + 1
+            // pstart + index - istart + 1 = 左边基础上+1（pstart + index - istart） + 1  不能index +1
             TreeNode rightNode = constructprein(pre, in, pstart + index - istart + 1, pend, index + 1, iend, inMap);
             rootNode.left = leftNode;
             rootNode.right = rightNode;
