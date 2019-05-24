1473a3999c89bbc8cd05d3671c6c08915f29f89c
xzy <18656432607@163.com>
2019-05-24 17:53:09 +0800

    add test code

---
+++ test/com/xzy/test/POJO.java
@@ -0,0 +1,37 @@
+package com.xzy.test;
+// created by xzy on 2019-05-22
+
+import java.util.Date;
+
+public class POJO {
+    private Integer id;
+
+    private String name;
+
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
