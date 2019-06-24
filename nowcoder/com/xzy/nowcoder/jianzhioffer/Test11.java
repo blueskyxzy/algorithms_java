032471f4fbd06278df028872beb45da0dd6ff277
xzy <blueskyxzy@gmail.com>
2019-06-24 18:22:32 +0800

    add test code

--- nowcoder/com/xzy/nowcoder/jianzhioffer/Test11.java
+++ nowcoder/com/xzy/nowcoder/jianzhioffer/Test11.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 // 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
 // 二进制中1的个数
+// 位运算
 public class Test11 {
     public static void main(String[] args) {
         System.out.println("请输入n:");
