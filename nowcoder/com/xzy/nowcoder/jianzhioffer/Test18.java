775f9a4d15d86791ae51f42e3aafa6660e4326f0
blueskyxzy <blueskyxzy@gmail.com>
2019-08-22 10:00:30 +0800

    test 二叉树的镜像

--- nowcoder/com/xzy/nowcoder/jianzhioffer/Test18.java
+++ nowcoder/com/xzy/nowcoder/jianzhioffer/Test18.java
@@ -38,7 +38,60 @@ public class Test18 {
         CodeUtils.midPrintBinaryTree(treeNodeA1);
     }
 
-    public static void Mirror(TreeNode root) {
+/*    public static void Mirror(TreeNode root) {
+        if (root != null){
+            TreeNode treeNode = new TreeNode(root.val);
+            midMirror(root, treeNode);
+//            root = treeNode;    // 自定义对象是引用传递，但是原来的内存地址的内存数据没有改变
+            exchangeRootData(treeNode, root);
+        } else {
+            // null, 不处理
+        }
+    }
 
+    private static void exchangeRootData(TreeNode treeNode, TreeNode root) {
+        if(treeNode != null){
+            if (root !=null){
+                root.val = treeNode.val;
+            } else {
+                TreeNode treeNode1 = new TreeNode(treeNode.val);
+                root = treeNode1;
+            }
+            exchangeRootData(treeNode.left, root.right);
+            exchangeRootData(treeNode.right, root.left);
+        } else {
+            root = null;
+        }
+    }
+
+    private static void midMirror(TreeNode root, TreeNode treeNode) {
+        if (root != null){
+            treeNode.val = root.val;
+            if (root.left != null){
+                TreeNode treeNodeLeft = new TreeNode(root.left.val);
+                treeNode.right = treeNodeLeft;
+            }
+            if (root.right != null){
+                TreeNode treeNodeRight= new TreeNode(root.right.val);
+                treeNode.left = treeNodeRight;
+            }
+            midMirror(root.left, treeNode.right);
+            midMirror(root.right, treeNode.left);
+        }
+    }*/
+
+    // 网上的方式  左右交换
+    public static void Mirror(TreeNode root) {
+        TreeNode temp;
+        if(root!=null)
+        {
+            temp=root.left;
+            root.left=root.right;
+            root.right=temp;
+            if(root.left!=null)
+                Mirror(root.left);
+            if(root.right!=null)
+                Mirror(root.right);
+        }
     }
 }
