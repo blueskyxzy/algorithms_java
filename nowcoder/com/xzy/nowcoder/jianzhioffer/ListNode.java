98939dca8d0bfcb2089c87d289d97b7dfdaa7372
xizy39282 <xizy39282@hundsun.com>
2023-01-31 15:52:11 +0800

    add leetcode

--- nowcoder/com/xzy/nowcoder/jianzhioffer/ListNode.java
+++ nowcoder/com/xzy/nowcoder/jianzhioffer/ListNode.java
@@ -2,10 +2,17 @@ package com.xzy.nowcoder.jianzhioffer;
 // created by xzy on 2019-06-25
 
 public class ListNode {
-    int val;
-    ListNode next = null;
+    public int val;
+    public ListNode next = null;
 
-    ListNode(int val) {
+    public ListNode(){}
+
+    public ListNode(int val) {
         this.val = val;
     }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+
 }
